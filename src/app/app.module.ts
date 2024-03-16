ee6cad80a999be586ff7d0c51cd12c1c07a6c39d
pablowinter <pablo.winter@gmail.com>
2024-03-16 19:26:01 -0300

    Primeros componentes en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { StudentsComponent } from './students/students.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    StudentsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
